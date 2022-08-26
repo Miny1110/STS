f6675ed9be1e5c42ced2db45ccd162297be76f8e
Miny1110 <wjdalswjd453@naver.com>
2022-08-26 12:22:51 +0900

    0826 오전

--- src/main/java/com/exe/springdi4/ServiceConsumer.java
+++ src/main/java/com/exe/springdi4/ServiceConsumer.java
@@ -3,10 +3,23 @@ package com.exe.springdi4;
 public class ServiceConsumer {
 
 	MessageService ms;
+	TimeService ts;
+	
+	/**메소드로 의존성 주입을 하기 위해서는 기본생성자 선언이 꼭 필요*/
+	public ServiceConsumer() {}
 	
 	/**생성자로 의존성 주입*/
 	public ServiceConsumer(MessageService ms) {
 		this.ms = ms;
+	}
+	
+	/**메소드로 의존성 주입
+	 * 메소드로 의존성 주입을 할 때에는 기본생성자가 꼭 필요해서 위에서 선언
+	 * 기본생성자로 객체생성을 한 후에 메소드가 실행되기 때문
+	 * 
+	 * app-context.xml 에서 매개변수를 넣어줘야 실행됨*/
+	public void setTimeService(TimeService ts) {
+		this.ts = ts;
 	}
 	
 	public void consumerService() {
@@ -18,6 +31,9 @@ public class ServiceConsumer {
 		
 		String message = ms.getMessage();
 		System.out.println(message);
+		
+		String time = ts.getTimeString();
+		System.out.println(time);
 	}
 	
 }
